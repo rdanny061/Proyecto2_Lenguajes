0040cb6e14a290f97de6d730f5de7ac65059cc6b
Danny Rojas Arguedas <rdanny061@gmail.com>
2019-04-08 21:54:23 -0600

    Arreglos en interfaz

--- src/hello_seesaw/core.clj
+++ src/hello_seesaw/core.clj
@@ -16,6 +16,9 @@
 (defn preguntaTipo2Frame [])
 
 (defn crearPreguntaUnica [nombreEncuesta listaEncuestas listaPreguntas]
+  
+  ;metodo para la creacion de preguntas tipo1(SeleccionUnica)
+  
   (let [inputCuerpoPregunta  (text :columns 20)
         valueInputCuerpoPregunta  (atom "")
         
@@ -48,6 +51,9 @@
       :on-close :exit)))
 
 (defn crearPreguntaEscala [nombreEncuesta listaEncuestas listaPreguntas]
+  
+  ;metodo para la creacion de preguntas tipo2(Escala)
+  
   (let [inputCuerpoPregunta  (text :columns 20)
         valueInputCuerpoPregunta  (atom "")
         
@@ -81,6 +87,9 @@
 
 
 (defn crearEncuesta [listaEncuestas]
+  
+  ;metodo para la creacion de encuestas
+  
   (let [inputNombreEncuesta  (text :columns 20)
         valueNombreEncuesta  (atom "")]
         
@@ -113,18 +122,21 @@
       :on-close :exit)))
 
 (defn preguntaTipo1 [listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador]
+  
+  ;metodo que despliega las preguntas de tipo1 (Seleccion Unica) y almacena su respuesta en la lista de respuestas
+  
   (let [input  (text :columns 20)
         combo (combobox :model (first(rest (first listaPreguntas2))))]
-    (frame 
+    (frame
+      :title "Pregunta Seleccion Única"
       :content 
       (vertical-panel 
-        :border 5
+        :border 150
         :items [
-                "Tipo1"
                 (str (first (first listaPreguntas2)))
                 
                 combo
-
+                
                 (action :name "Siguiente"
                   :handler (fn [e] (dispose! (all-frames)) (invoke-later
                                                              (-> (validacionTipo listaEncuestas listaPreguntas1 (rest listaPreguntas2) (cons (reverse (cons "tipo1" (reverse (cons (first (first listaPreguntas2)) (list (selection combo)))))) listaRespuestas) contador)
@@ -134,16 +146,26 @@
       :on-close :exit)))
 
 (defn preguntaTipo2Frame [listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador listaItems listaRespuestasItems]
+  
+  ;metodo que realiza la respuesta de preguntas, despliega la pregunta si es tipo2(Escala) con las posibles opciones en un combo
+  ;esto lo hace por items de cada pregunta
+  
   (let [input  (text :columns 20)
         combo (combobox :model ["Muy bien" "Bien" "Regular" "Malo" "Muy malo"])]
-    (frame 
+    (frame
+      :title "Pregunta en Escala"
       :content 
       (vertical-panel 
-        :border 5
-        :items ["Tipo2"
+        :border 100
+        :items [
+                "Pregunta:"
                 (str (first (first listaPreguntas2)))
+                
+                "Item a evaluar:"
                 (str (first listaItems))
+                
                 combo
+                
                 (action :name "Siguiente"
                   :handler (fn [e] (dispose! (all-frames)) (invoke-later
                                                              (-> (preguntaTipo2 listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador (rest listaItems) (cons (cons (first listaItems) (list (selection combo))) listaRespuestasItems))
@@ -152,17 +174,25 @@
       :on-close :exit)))
 
 (defn preguntaTipo2 [listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador listaItems listaRespuestasItems]
+  
+  ;metodo que verifica la cantidad de items de las preguntas de Tipo2(Escala), cuando ya no existen agrega las respuestas
+  
   (cond
+    
     (not(empty? listaItems)) (invoke-later
                                (-> (preguntaTipo2Frame listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador listaItems listaRespuestasItems)
                                    pack!
                                    show!)) 
+    
     (empty? listaItems) (invoke-later
                           (-> (validacionTipo listaEncuestas listaPreguntas1 (rest listaPreguntas2) (cons (reverse (cons "tipo2" (reverse(cons (first (first listaPreguntas2)) (list listaRespuestasItems))))) listaRespuestas) contador)
                               pack!
                               show!))))
 
 (defn responderEncuesta [listaEncuestas]
+  
+  ;metodo para responder encuestas, pide la encuesta que sera respondida y la cantidad de veces que se quiere responder
+  
   (let [inputContador  (text :columns 20)
         valueContador  (atom "")
         combo (combobox :model (map #(first %) listaEncuestas))
@@ -199,29 +229,33 @@
                                                                  pack!
                                                                  show!))))]))))
 
-(defn validacionTipo [listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador]
+(defn validacionTipo [listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador] 
+  
+  ;metodo que valida al responder las preguntas si es tipo1 (Seleccion unica) y tipo2 (escala), aparte de esto verifica si ya no existen
+  ;preguntas en esa encuesta y la cantidad de veces que se responde cada encuesta
 
   (cond
+    
     (= contador 0) (invoke-later
                      (-> (estadistica listaEncuestas listaRespuestas)
                          pack!
                          show!))
+    
     (not(empty? listaPreguntas2)) (cond
+                                    
                                     (= (str (first (reverse (first listaPreguntas2)))) "tipo1") (invoke-later
-                                                                                                  (-> (preguntaTipo1 listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador) ;lista 2 es la que va a cambiar
+                                                                                                  (-> (preguntaTipo1 listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador)
                                                                                                       pack!
                                                                                                       show!))
+                                    
                                     (= (str (first (reverse (first listaPreguntas2)))) "tipo2") (invoke-later
-                                                                                                  (-> (preguntaTipo2 listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador (first(rest (first listaPreguntas2))) '()) ;lista 2 es la que va a cambiar
+                                                                                                  (-> (preguntaTipo2 listaEncuestas listaPreguntas1 listaPreguntas2 listaRespuestas contador (first(rest (first listaPreguntas2))) '())
                                                                                                       pack!
                                                                                                       show!)))
     (empty? listaPreguntas2) (invoke-later
                                (-> (validacionTipo listaEncuestas listaPreguntas1 listaPreguntas1 listaRespuestas (- contador 1))
                                    pack!
                                    show!))))
-    
-    
-    ;se necesita que la preguntaTipo# llame con rest
 
 (defn estadistica [listaEncuestas listaRespuestas]
   (let [input  (text :columns 20)]
@@ -236,6 +270,9 @@
       :on-close :exit)))
 
 (defn tipoPregunta [nombreEncuesta listaEncuestas listaPreguntas]
+  
+  ;metodo que pregunta que tipo de pregunta desea crear y redirige hacia la creacion del tipo elegido
+  
   (let []
     (frame
       :title "Tipos de preguntas"
@@ -268,6 +305,9 @@
       :on-close :exit)))
 
 (defn mainForm [listaEncuestas]
+  
+  ;ventana principal, esta pregunta que desea realizar el usuario, ya sea crear o responder encuestas
+  
   (let []
     (frame
       :title "Ventana Principal"
